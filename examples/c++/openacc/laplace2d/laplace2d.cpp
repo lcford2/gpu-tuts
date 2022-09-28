eb1a5799e962eb2be1d25c78ecad4aa4095ab78e
Lucas Ford <lcford2@ncsu.edu>
2022-09-28 10:58:30 -0400

    fast gpu accel for laplace

--- examples/c++/openacc/laplace2d/laplace2d.cpp
+++ examples/c++/openacc/laplace2d/laplace2d.cpp
@@ -48,6 +48,103 @@ void serial(long rows, long columns)
     cout << "Serial Time: " << elapsed.count() / 1000 << " seconds\n";
 }
 
+void bad_acceleration(long rows, long columns)
+{
+    float A[rows][columns];
+    float A_new[rows][columns];
+
+    // init matrices
+    for (int i=0; i<rows; ++i) {
+        for (int j=0; j<columns; ++j) {
+            A[i][j] = 0.0;
+            A_new[i][j] = 0.0;
+        }
+    }
+    // give A a spike in the middle
+    A[rows/2][columns/2] = 2.0;
+
+    // setup jacobi information
+    float dt=1.0, max_error=1e-4;
+    int iter=0, max_iter=1000;
+
+    // start timer
+    auto start_time = chrono::high_resolution_clock::now();
+    // loop until error critera or iters is reached
+    // the outer loop cannot be accelerated because
+    // of the data dependency on dt
+    while (dt > max_error && iter <= max_iter) {
+#pragma acc kernels
+        for (int i=1; i<rows-1; ++i) {
+            for (int j=1; j<columns-1; ++j) {
+                A_new[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j+1] + A[i][j-1]);
+            }
+        }
+
+        dt = 0.0;
+#pragma acc parallel loop reduction(max:dt)
+        for (int i=1; i<rows-1; ++i) {
+            for (int j=1; j<columns-1; ++j) {
+                dt = fmax(fabs(A_new[i][j] - A[i][j]), dt);
+                A[i][j] = A_new[i][j];
+            }
+        }
+        ++iter;
+    }
+    auto stop_time = chrono::high_resolution_clock::now();
+
+    chrono::duration<float, milli> elapsed = stop_time - start_time;
+    cout << "Bad Acceleration Time: " << elapsed.count() / 1000 << " seconds\n";
+}
+
+void good_acceleration(long rows, long columns)
+{
+    float A[rows][columns];
+    float A_new[rows][columns];
+
+    // init matrices
+    for (int i=0; i<rows; ++i) {
+        for (int j=0; j<columns; ++j) {
+            A[i][j] = 0.0;
+            A_new[i][j] = 0.0;
+        }
+    }
+    // give A a spike in the middle
+    A[rows/2][columns/2] = 2.0;
+
+    // setup jacobi information
+    float dt=1.0, max_error=1e-4;
+    int iter=0, max_iter=1000;
+
+    // start timer
+    auto start_time = chrono::high_resolution_clock::now();
+    // loop until error critera or iters is reached
+    // the outer loop cannot be accelerated because
+    // of the data dependency on dt
+#pragma acc data copy(A) create(A_new) copy(dt) copy(max_error) copy(iter) copy(max_iter)
+    while (dt > max_error && iter <= max_iter) {
+#pragma acc kernels
+        for (int i=1; i<rows-1; ++i) {
+            for (int j=1; j<columns-1; ++j) {
+                A_new[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j+1] + A[i][j-1]);
+            }
+        }
+
+        dt = 0.0;
+#pragma acc parallel loop reduction(max:dt)
+        for (int i=1; i<rows-1; ++i) {
+            for (int j=1; j<columns-1; ++j) {
+                dt = fmax(fabs(A_new[i][j] - A[i][j]), dt);
+                A[i][j] = A_new[i][j];
+            }
+        }
+        ++iter;
+    }
+    auto stop_time = chrono::high_resolution_clock::now();
+
+    chrono::duration<float, milli> elapsed = stop_time - start_time;
+    cout << "Good Acceleration Time: " << elapsed.count() / 1000 << " seconds\n";
+}
+
 
 int main(int argc, char **argv)
 {
@@ -62,5 +159,6 @@ int main(int argc, char **argv)
 
     cout << "Grid Size: [" << rows << ", " << columns << "]\n";
     serial(rows, columns);
-
+    bad_acceleration(rows, columns);
+    good_acceleration(rows, columns);
 }
